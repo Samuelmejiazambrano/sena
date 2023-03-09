bd7c46d5b889040cbe8a5f341ee3e5ded7a5efe9
samuelmejia <samuelmejiaz2005@gamail.com>
2023-03-09 15:50:12 -0500

    segundo archivo

---
+++ nuevo.js
@@ -0,0 +1,63 @@
+
+//var resultado=pr1*pr2;
+//console.log("el resultado es:"  +  resultado);
+
+//condicionales
+
+  //let miEdad=18;
+
+ // if (miEdad>=18) {
+    // console.log("puedes ingresar");
+  //}
+  //else{
+   // console.log(" numero no valido");
+  //};//
+
+ // let contador=1;
+  //while(contador<51) {
+    //console.log(contador);
+    //contador=contador + 1;
+  //};
+  //for (let contador = 100; contador>=0; contador--) {
+    //console.log(contador);
+
+   // let contar=0;
+    //while(contar<=205){
+      //console.log(contar);
+       // contar=contar +1;
+       
+        
+    //};
+  
+   // for (let hola = 0; hola <= 10; hola++) {
+      //console.log(hola);
+      
+   // }
+
+    //function suma(a,b=0) {
+
+      //console.log(a+b);
+      
+   // }
+    //suma(2+4);
+
+   // var miAuto ={
+
+      // marca:"Ford" ,
+      // modelo:"corolla",
+      // ano:2002
+   // };
+    let h1= document.querySelector('#h1')
+    let input5= document.querySelector('#input5 ');
+    let input2=document.querySelector('#input2');
+    let btn= document.querySelector('#btn');
+
+    function btnOnclick(){
+    console.log(input5.value + input2.value);
+    };
+  
+    btnOnclick();
+    
+    
+      
+    
